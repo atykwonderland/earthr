c4c1bfb790b661bbca49f3bc8c75f86feb07931a
taerim25 <taerim25@gmail.com>
2020-02-02 03:43:24 -0500

    prototype created

--- my-app/src/FoodResult.jsx
+++ my-app/src/FoodResult.jsx
@@ -5,8 +5,10 @@ class FoodResult extends Component {
         const {data} = this.props.location.state
         return (
             <div>
-                <p>food</p>
-                <p>{data['carbon-footprint_100g']}</p>
+                
+                <p>this food {data['product_name']}, also known as {data['generic_name']}, is served in the quantity of {data['serving_size']} per serving. 
+                    the quantiy of this food comes in {data['quantity']} per packaging and the consumption of this product outputs a total carbon-footprint,
+                    including everything from production to distribution of the product, is {data['carbon-footprint_100g']}  </p>
             </div>
         )
     }
